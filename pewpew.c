283519bb2078d36108272fba746ae960f0f031d2
John Andersen <johnandersenpdx@gmail.com>
2017-04-20 17:25:36 -0700

    PCI, cdev, and user.c control LEDCTL
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- pewpew.c
+++ pewpew.c
@@ -41,7 +41,6 @@ struct pewpew_dev {
   unsigned int count;
   char name[255];
   struct cdev cdev;
-  int syscall_val;
   struct pci_dev *pdev;
   void *addr;
 };
@@ -66,10 +65,6 @@ struct file_operations pewpew_fops = {
   .write = pewpew_write,
   .release = pewpew_release,
 };
-
-static int syscall_val = 40;
-module_param(syscall_val, int, S_IRUSR|S_IWUSR);
-MODULE_PARM_DESC(syscall_val, "Initial value of syscall_val");
 
 int pewpew_probe(struct pci_dev *pdev, const struct pci_device_id *ent) {
   int bars, err;
@@ -110,21 +105,9 @@ int pewpew_probe(struct pci_dev *pdev, const struct pci_device_id *ent) {
     goto err_ioremap;
   }
 
+  /* Save the PCI device for later use */
   pewpew.pdev = pdev;
-
-  /* Clear it all (probably bad) */
-  *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)) = 0;
-  /* Turn on LED0 */
-  *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)) |= (u32)(
-      DEV_82583V_LEDCTL_LED0(DEV_82583V_LEDCTL_MODE_ACTIVE|
-          DEV_82583V_LEDCTL_BLINK_MODE|DEV_82583V_LEDCTL_IVRT|
-          DEV_82583V_LEDCTL_BLINK));
-  /* Turn on LED1 */
-  *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)) |= (u32)(
-      DEV_82583V_LEDCTL_LED1(DEV_82583V_LEDCTL_MODE_ACTIVE|
-          DEV_82583V_LEDCTL_BLINK_MODE|DEV_82583V_LEDCTL_IVRT|
-          DEV_82583V_LEDCTL_BLINK));
-  /* Display the new value */
+  /* Display the value of LEDCTL */
   printk(INFO "LEDCTL: %08x\n", *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)));
   return 0;
 
@@ -137,21 +120,20 @@ err_pci_reg:
 }
 
 void pewpew_remove(struct pci_dev *pdev) {
-  if (pdev != NULL) {
-    /* Clear it all (probably bad) */
-    *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)) = 0;
-    iounmap(pewpew.addr);
-    pci_release_mem_regions(pdev);
-    pci_disable_device(pdev);
-    pewpew.pdev = NULL;
-    pewpew.addr = NULL;
-  }
+  /* Clear it all (probably bad) */
+  *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)) = 0;
+  iounmap(pewpew.addr);
+  pci_release_mem_regions(pdev);
+  pci_disable_device(pdev);
+  pewpew.pdev = NULL;
+  pewpew.addr = NULL;
 }
 
 int pewpew_open(struct inode *inode, struct file *flip) {
-	flip->private_data = container_of(inode->i_cdev, struct pewpew_dev, cdev);
-  ((struct pewpew_dev *)flip->private_data)->syscall_val = syscall_val;
-  printk(INFO "Open starting syscall_val at %d\n", syscall_val);
+  /* Make sure we are connected to the PCI device */
+  if (pewpew.pdev == NULL || pewpew.addr == NULL) {
+    return -ENODEV;
+  }
 	return 0;
 }
 
@@ -159,7 +141,10 @@ ssize_t pewpew_read(struct file *flip, char __user *buff, size_t count, loff_t *
   int err;
   char *kbuff;
   const unsigned int kbuff_size = 255;
-  struct pewpew_dev *p = flip->private_data;
+  /* Make sure we are connected to the PCI device */
+  if (pewpew.pdev == NULL || pewpew.addr == NULL) {
+    return -ENODEV;
+  }
   /* Make sure we got a buffer from userspace */
   if (buff == NULL) {
     printk(ERR "NULL buffer from userspace\n");
@@ -172,23 +157,32 @@ ssize_t pewpew_read(struct file *flip, char __user *buff, size_t count, loff_t *
     printk(ERR "failed to allocate kernel buffer\n");
     return -ENOMEM;
   }
-  /* Format syscall_val into the kernel buffer */
-  err = sprintf(kbuff, "%d", p->syscall_val);
+  /* Format LEDCTL into the kernel buffer */
+  err = sprintf(kbuff, "%x", *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)));
+  /* Make sure we don't write back a string larger than the receiving
+   * buffer can hold */
+  if (count < strnlen(kbuff, kbuff_size)) {
+    return -ENOMEM;
+  }
   /* Find the length of the string we are writing back */
   count = strnlen(kbuff, kbuff_size);
   /* Copy the contents of the user buffer to the kernel buffer */
   copy_to_user(buff, kbuff, count);
   /* Free the kernel buffer */
   kfree(kbuff);
-  printk(INFO "Read complete, syscall_val was %d\n", p->syscall_val);
+  printk(INFO "Read complete, LEDCTL is %x\n", *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)));
   /* Return length of the string written back */
   return count;
 }
 
 ssize_t pewpew_write(struct file *flip, const char __user *buff, size_t count, loff_t *offp) {
   int err;
+  u32 led_val;
   char *kbuff;
-  struct pewpew_dev *p = flip->private_data;
+  /* Make sure we are connected to the PCI device */
+  if (pewpew.pdev == NULL || pewpew.addr == NULL) {
+    return -ENODEV;
+  }
   /* Make sure we got a buffer from userspace */
   if (buff == NULL) {
     printk(ERR "NULL buffer from userspace\n");
@@ -204,8 +198,10 @@ ssize_t pewpew_write(struct file *flip, const char __user *buff, size_t count, l
   /* Copy the contents of the user buffer to the kernel buffer */
   copy_from_user(kbuff, buff, (sizeof(*buff) * count) + 1);
   kbuff[sizeof(*buff) * count] = '\0';
-  /* Parse the kernel buffer into the pewpew_dev syscall_val */
-  err = kstrtoint(kbuff, 10, &p->syscall_val);
+  /* Parse the kernel buffer into led_val */
+  err = kstrtoint(kbuff, 16, &led_val);
+  /* Set LEDCTL to 32-bit led_val */
+  *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)) = led_val;
   /* Free the kernel buffer */
   kfree(kbuff);
   /* Check if parse was successful */
@@ -213,7 +209,7 @@ ssize_t pewpew_write(struct file *flip, const char __user *buff, size_t count, l
     printk(ERR "failed to parse integer\n");
     return -EINVAL;
   }
-  printk(INFO "Write complete, syscall_val is now %d\n", p->syscall_val);
+  printk(INFO "Write complete, LEDCTL is %x\n", *((u32 *)(pewpew.addr + DEV_82583V_LEDCTL)));
   /* Return length that was given on success */
   return count;
 }
@@ -228,9 +224,6 @@ static int __init pewpew_init(void) {
   /* Initialize our device structure */
   memset(&pewpew, 0, sizeof(pewpew));
   pewpew.count = 1;
-  /* Get value from module parameter. */
-  pewpew.syscall_val = syscall_val;
-  printk(INFO "syscall_val starting at %d\n", pewpew.syscall_val);
   strcpy(pewpew.name, MODULE_STR);
   /* Obtain a device number */
   err = alloc_chrdev_region(&pewpew.dev, 0, pewpew.count, pewpew.name);
@@ -260,8 +253,8 @@ static void __exit pewpew_exit(void) {
   cdev_del(&pewpew.cdev);
   /* Unregister our device number */
   unregister_chrdev_region(pewpew.dev, pewpew.count);
-  /* Unregister the PCI device */
-  pewpew_remove(pewpew.pdev);
+  /* Unregister our PCI driver */
+  pci_unregister_driver(&pewpew_driver);
   printk(INFO "Exited successfully\n");
 }
 
--- user.c
+++ user.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -9,15 +10,19 @@
 
 #define DEV_NAME "/dev/pewpew0"
 
-int main(int argc, char **argv) {
+#define DEV_82583V_LEDCTL_MODE_ACTIVE         0x0E
+#define DEV_82583V_LEDCTL_BLINK_MODE          (1 << 5)
+#define DEV_82583V_LEDCTL_IVRT                (1 << 6)
+#define DEV_82583V_LEDCTL_BLINK               (1 << 7)
+#define DEV_82583V_LEDCTL_LED0(X)             ((X) << 0)
+#define DEV_82583V_LEDCTL_LED1(X)             ((X) << 8)
+
+
+int main() {
   int fd, n;
+  uint32_t led_val;
   const unsigned int buff_size = 255;
   char buff[buff_size];
-
-  if (argc != 2) {
-    printf("Usage: %s number\n", argv[0]);
-    return EXIT_FAILURE;
-  }
 
   fd = open(DEV_NAME, O_RDWR);
   if (fd < 0) {
@@ -30,9 +35,18 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  printf("%s\n", buff);
+  printf("LEDCTL: %s\n", buff);
 
-  if (write(fd, argv[1], strlen(argv[1])) != strlen(argv[1])) {
+  /* Turn on LED0 */
+  led_val = (uint32_t)(
+      DEV_82583V_LEDCTL_LED0(DEV_82583V_LEDCTL_MODE_ACTIVE|
+          DEV_82583V_LEDCTL_BLINK_MODE|DEV_82583V_LEDCTL_IVRT|
+          DEV_82583V_LEDCTL_BLINK));
+
+  memset(buff, 0, buff_size);
+  sprintf(buff, "%x", led_val);
+
+  if (write(fd, buff, strlen(buff)) != strlen(buff)) {
     perror("Error writing to " DEV_NAME);
     return EXIT_FAILURE;
   }
@@ -43,7 +57,28 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  printf("%s\n", buff);
+  printf("LEDCTL: %s\n", buff);
+
+  sleep(2);
+
+  /* Turn off LED0 */
+  led_val = 0;
+
+  memset(buff, 0, buff_size);
+  sprintf(buff, "%x", led_val);
+
+  if (write(fd, buff, strlen(buff)) != strlen(buff)) {
+    perror("Error writing to " DEV_NAME);
+    return EXIT_FAILURE;
+  }
+
+  memset(buff, 0, buff_size);
+  if (read(fd, buff, buff_size) < 1) {
+    perror("Error reading from " DEV_NAME);
+    return EXIT_FAILURE;
+  }
+
+  printf("LEDCTL: %s\n", buff);
 
   return EXIT_SUCCESS;
 }
